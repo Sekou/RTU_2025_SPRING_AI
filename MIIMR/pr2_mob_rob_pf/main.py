72f0d5e125bf0a4e45d2640516cbdbe5198b8a12
Sekou <sekoudiane1990@gmail.com>
2025-02-19 17:19:11 +0300

    Added practice code

--- MIIMR/pr2_mob_rob_pf/main.py
+++ MIIMR/pr2_mob_rob_pf/main.py
@@ -1,207 +1,220 @@
-import pygame, sys, math, numpy as np
-winSize=(800,600)
+
+import sys, pygame
+import numpy as np
+import math
+
+import pygame.draw
 
 pygame.font.init()
-def drawText(screen, x, y, text, sz=25):
+def drawText(screen, s, x, y, sz=20, color=(0,0,0)): #отрисовка текста
     font = pygame.font.SysFont('Comic Sans MS', sz)
-    screen.blit(font.render(text,  False, (0, 0, 0)), (x, y))
+    surf=font.render(s, True, (0,0,0))
+    screen.blit(surf, (x,y))
 
-#[[0,350],[350,450],[450,800]], [0, 1, 2]
-def discretize(val, ranges, vals):
-    for i, r in enumerate(ranges):
-        if r[0]<=val<r[1]:
-            return vals[i]
+def rot(v, ang): #поворот вектора на угол
+    s, c = math.sin(ang), math.cos(ang)
+    return [v[0] * c - v[1] * s, v[0] * s + v[1] * c]
 
-class Cart:
-    def __init__(self):
-        self.x,self.v,self.a=winSize[0]/2, 0,0 #положение, скорость, ускорение
-        self.control=0
-        self.y=0
-        self.gamma=0.01 #угол балки
-        self.dgamma_dt=0 #скорость падения
-        self.dgamma_d2t=0 #ускорение падения
-        self.barL=70 #длина балки
+def limAng(ang): #ограничение угла в пределах +/-pi
+    while ang > math.pi: ang -= 2 * math.pi
+    while ang <= -math.pi: ang += 2 * math.pi
+    return ang
+
+def rotArr(vv, ang): # функция для поворота массива на угол
+    return [rot(v, ang) for v in vv]
+
+def dist(p1, p2): #расстояние между точками
+    return np.linalg.norm(np.subtract(p1, p2))
+
+def drawRotRect(screen, color, pc, w, h, ang): #точка центра, ширина высота прямоуг и угол поворота прямогуольника
+    pts = [
+        [- w/2, - h/2],
+        [+ w/2, - h/2],
+        [+ w/2, + h/2],
+        [- w/2, + h/2],
+    ]
+    pts = rotArr(pts, ang)
+    pts = np.add(pts, pc)
+    pygame.draw.polygon(screen, color, pts, 2)
+
+sz = (800, 600)
+
+class Tree:
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+        self.color=(0,125,0)
+    def get_pos(self):
+        return [self.x, self.y]
     def draw(self, screen):
-        y=winSize[1]*2/3
-        pygame.draw.line(screen, (0,0,0),(0,y), (winSize[0],y), 1)
-        self.y=y-25
-        w,h=60,35
-        x1,x2=self.x-w/2, self.x+w/2
-        y1,y2=self.y-h/2, self.y+h/2
-        pygame.draw.rect(screen, (0,0,0), [x1, y1, x2-x1, y2-y1], 1)
-        wheelX1, wheelX2 = self.x-w/3, self.x+w/3
-        wheelY1 = wheelY2 = self.y+h/2
-        pygame.draw.circle(screen, (0,0,0), [wheelX1, wheelY1],10, 1)
-        pygame.draw.circle(screen, (0,0,0), [wheelX2, wheelY2],10, 1)
-        barX, barY = self.x, self.y-h/2
-        s,c=math.sin(self.gamma+math.pi/2), math.cos(self.gamma+math.pi/2)
-        barX2, barY2 = barX+self.barL*c, barY-self.barL*s
-        pygame.draw.line(screen, (0,0,0),(barX, barY), (barX2, barY2), 1)
+        p1 = np.array(self.get_pos())
+        p2 = p1+[0,-35]
+        pygame.draw.line(screen, self.color, p1, p2, 2)
+        self.draw_branch(screen, p2, 10, 0.7, 0)
+        self.draw_branch(screen, p2, 15, 0.7, 8)
+        self.draw_branch(screen, p2, 20, 0.7, 16)
+        self.draw_branch(screen, p2, 10, -0.7+math.pi, 0)
+        self.draw_branch(screen, p2, 15, -0.7+math.pi, 8)
+        self.draw_branch(screen, p2, 20, -0.7+math.pi, 16)
+    def draw_branch(self, screen, p2, l, a, dy):
+        s, c = math.sin(a), math.cos(a)
+        pygame.draw.line(screen, self.color, p2+[0,dy], p2+[l*c, l*s+dy], 2)
+
+class Landmark:
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+    def get_pos(self):
+        return [self.x, self.y]
+    def draw(self, screen):
+        p1 = np.array(self.get_pos())
+        pygame.draw.circle(screen, (0,0,0), p1, 3, 2)
+
+class Particle:
+    def __init__(self, x, y):
+        self.Q = 0
+        self.x, self.y = x, y
+        self.color = (0,0,255)
+    def get_pos(self):
+        return [self.x, self.y]
+    def draw(self, screen):
+        p1 = np.array(self.get_pos())
+        pygame.draw.circle(screen, self.color, p1, 3, 2)
+
+class ParticleFilter:
+    def __init__(self, x0, y0, num=30, sigma=20):
+        self.particles=[]
+        self.num=num
+        self.sigma=sigma
+        for i in range(num):
+            x_=np.random.normal(x0, self.sigma)
+            y_=np.random.normal(y0, self.sigma)
+            self.particles.append(Particle(x_, y_))
+    def draw(self, screen):
+        for p in self.particles:
+            p.draw(screen)
+    def estimate(self, robot_pos, robot_ang, lm_pos, measured_dist, measured_ang):
+        for p in self.particles:
+            etalon_dist=dist(p.get_pos(), robot_pos)
+            delta=measured_dist-etalon_dist
+            x1, y1, x2, y2 = [*robot_pos, *lm_pos]
+            a2 = math.atan2(y2 - y1, x2 - x1)
+            etalon_ang = limAng(a2 - robot_ang)
+            delta2=measured_ang-etalon_ang
+            d=delta*0.1 + delta2*1
+            p.Q=np.exp(-d*d*0.01)
+            p.color=(0, 0, int(255*p.Q))
+    def repopulate(self):
+        self.particles=sorted(self.particles, key=lambda p: p.Q)
+        self.particles=self.particles[self.num//2:]
+        while len(self.particles)<self.num:
+            i=np.random.randint(len(self.particles))
+            p=self.particles[i]
+            p2=Particle(np.random.normal(p.x, 5), np.random.normal(p.y, 5))
+            self.particles.append(p2)
+    def getResult(self):
+        pp=[p.get_pos() for p in self.particles]
+        return np.mean(pp, axis=0)
+
+class Robot:
+    def __init__(self, x, y):
+        self.radius=15
+        self.color=(0,0,0)
+        self.x, self.y, self.a=x,y,0
+        self.vlin, self.vrot=0,0
+        self.observerd_lm=None #ориентир
+        self.measurm_err_d=3 #ошибка измерения дальности
+        self.measurm_err_a=0.05 #ошибка измерения угла
+        self.last_measurm_d = 0
+        self.last_measurm_a = 0
+    def get_pos(self):
+        return [self.x, self.y]
+    def get_expected_lm_pos(self):
+        p1=np.array(self.get_pos())
+        a_ = self.a + self.last_measurm_a
+        return p1 + [math.cos(a_) * self.last_measurm_d, math.sin(a_) * self.last_measurm_d]
+    def draw(self, screen):
+        p1=np.array(self.get_pos())
+        pygame.draw.circle(screen, self.color, p1, self.radius, 2)
+        s,c=math.sin(self.a), math.cos(self.a)
+        pygame.draw.line(screen, self.color, p1, p1+[self.radius*c, self.radius*s],2)
+        if self.observerd_lm:
+            p2=self.get_expected_lm_pos()
+            pygame.draw.line(screen, (180, 180, 180), p1, p2, 1)
+            drawText(screen, f"{self.last_measurm_d:.0f}", *(0.5 * (p1 + p2)))
     def sim(self, dt):
+        s,c=math.sin(self.a), math.cos(self.a)
+        self.x+=c*self.vlin*dt
+        self.y+=s*self.vlin*dt
+        self.a+=self.vrot*dt
 
-        self.a=0.9*self.control
-        self.v+=self.a * dt
-        self.x += self.v * dt
+    def measure_dist(self, lm):
+        d = dist(self.get_pos(), lm.get_pos()) + np.random.normal(0, self.measurm_err_d)
+        self.last_measurm_d = d
+        return d
+    def measure_ang(self, lm):
+        x1, y1, x2, y2=[*self.get_pos(), *lm.get_pos()]
+        a1=self.a
+        a2=math.atan2(y2-y1, x2-x1)
+        da=limAng(a2-a1)
+        da+=np.random.normal(0, self.measurm_err_a)
+        self.last_measurm_a = da
+        return da
 
-        self.dgamma_d2t=0.9*self.gamma + 0.01*self.a
-        self.dgamma_dt+=self.dgamma_d2t * dt
-        self.gamma += self.dgamma_dt * dt
-        maxAng=1
-        if self.gamma>maxAng: self.gamma=maxAng
-        if self.gamma<-maxAng: self.gamma=-maxAng
+def sim(robot, lm, dt):
+    robot.sim(dt)
+    d = robot.measure_dist(lm)
+    a = robot.measure_ang(lm)
 
-    def controlExpert(self):
-        dxCart=winSize[0]/2-self.x
-        if self.gamma<-0.01:
-            self.control=100
-            if dxCart>0 and abs(self.gamma)>0.3:  self.control+= 0.1*dxCart
-        elif self.gamma>0.01:
-            self.control=-100
-            if dxCart<0 and abs(self.gamma)>0.3:  self.control+= 0.1*dxCart
+def main():
+    screen = pygame.display.set_mode(sz)
+    timer = pygame.time.Clock()
+    fps = 20
+    dt = 1 / fps
 
-        else:
-            self.control=0
+    robot = Robot(200, 200)
+    lm = Landmark(500, 300)
+    sim(robot, lm, dt)
+    pf = ParticleFilter(*robot.get_expected_lm_pos())
 
-class QTable:
-    def __init__(self):
-        self.policy=dict()
-    def createPolicy(self, history, statesVariants, actionsVariants):
-        for sv in statesVariants:
-            for av in actionsVariants:
-                q=history.calcQ(sv, av)
-                self.policy[f"{sv}+{av}"]=q
-
-    def query(self, s):
-        res=[[key,self.policy[key]] for key in self.policy.keys() if key.startswith(str(s)+"+")]
-        return res
-
-    def selectAction(self, s):
-        variants=self.query(s)
-        if len(variants)==0: return 1 #TODO: fix magic number
-        aa=[v[0] for v in variants]
-        aa=[a[a.index("+")+1:] for a in aa]
-        qq=[v[1] for v in variants]
-        i = np.argmax(qq)
-        return int(aa[i])
-
-class History:
-    def __init__(self):
-        self.records=[]
-    def addRecord(self, s, a, r):
-        self.records.append([len(self.records), s, a, r])
-    def query(self, s, a):
-        rr=[r for r in self.records if r[1]==s and r[2]==a]
-        return rr
-    def calcQ(self, s, a, horizon=10):
-        rr=self.query(s, a)
-        if len(rr)==0: return 0.5
-        qq=[]
-        for r in rr:
-            q=0
-            i0=r[0]
-            discount0=0.7
-            i1=min(i0+horizon, len(self.records))
-            for i in range(i0, i1):
-                discount=math.pow(discount0, i-i0)
-                q += discount * self.records[i][-1]
-            qq.append(q)
-        return np.mean(qq)
-
-if __name__ == "__main__":
-    cart=Cart()
-    fps=30
-    dt=1/fps
-    screen=pygame.display.set_mode(winSize)
-    timer=pygame.time.Clock()
-
-    history = History() #таблица исторических данных
-    qtable = QTable() #таблица политики
-
-    MODE="manual" #or "auto"
+    robot.observerd_lm=lm
+    tree=Tree(500, 300)
 
     while True:
-
-        xDiscr=discretize(cart.x, [[0,350],[350,450],[450,800]], [0, 1, 2])
-        gammaDiscr=discretize(cart.gamma, [[-math.pi/2,-0.03],[-0.03,0.03],[0.03,math.pi/2]], [0, 1, 2])
-        state = 10 * xDiscr + gammaDiscr
-        action=discretize(cart.v, [[-300,-20],[-20,20],[20,300]], [0, 1, 2])
-        reward=1/(5*abs(cart.gamma)+1)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+        for ev in pygame.event.get():
+            if ev.type==pygame.QUIT:
                 sys.exit(0)
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_a:
-                    cart.control=-100
-                if event.key == pygame.K_d:
-                    cart.control=100
-                if event.key == pygame.K_m:
-                    MODE="auto" if MODE=="manual" else "manual"
-                if event.key == pygame.K_1:
-                    historySubset=history.query(11, 2)
-                    print(historySubset)
-                if event.key == pygame.K_2:
-                    Q=history.calcQ(11, 2)
-                    print(Q)
-                if event.key == pygame.K_3:
-                    #TODO: автоматизировать формирование комбинаций
-                    qtable.createPolicy(history, [0, 1, 2, 10, 11, 12, 20,21,22], [0, 1, 2])
-                    print(qtable.policy)
-                if event.key == pygame.K_4:
-                    actionsSubset=qtable.query(state)
-                    print(actionsSubset)
-                if event.key == pygame.K_5:
-                   MODE="qtable"
-                if event.key == pygame.K_r:
-                   cart.x=400
+            if ev.type == pygame.KEYDOWN:
+                if ev.key == pygame.K_w:
+                    robot.vlin=50
+                    robot.vrot = 0
+                if ev.key == pygame.K_s: robot.vlin=-50
+                if ev.key == pygame.K_a: robot.vrot=-1
+                if ev.key == pygame.K_d: robot.vrot=+1
+                if ev.key == pygame.K_z: robot.vrot = robot.vlin = 0
+                if ev.key == pygame.K_1:
+                    pf.estimate(robot.get_pos(), robot.a, lm.get_pos(), robot.measure_dist(lm), robot.measure_ang(lm))
+                if ev.key == pygame.K_2:
+                    pf.repopulate()
 
-        if MODE=="auto":
-            cart.controlExpert()
-            s=xDiscr*10+gammaDiscr
-            history.addRecord(s, action, reward)
-        if MODE=="qtable":
-            a=qtable.selectAction(s)
-            print(s, "->", a)
-            if a==0:
-                cart.control = -100
-            if a==1:
-                cart.control = 0
-            if a==2:
-                cart.control = 100
+        sim(robot, lm, dt)
+        pf.estimate(robot.get_pos(), robot.a, lm.get_pos(), robot.measure_dist(lm), robot.measure_ang(lm))
 
-        cart.sim(dt)
-        screen.fill((255,255,255))
-        #1
-        drawText(screen, 5, 5, f"x={cart.x:.1f}")
-        #2
-        drawText(screen, 5, 25, f"x*={xDiscr}")
-        #3
-        drawText(screen, 5, 45, f"gamma={cart.gamma:.2f}")
-        #4
-        drawText(screen, 5, 65, f"gamma*={gammaDiscr}")
-        #5
-        drawText(screen, 5, 85, f"action*={action}")
-        #6
-        drawText(screen, 5, 105, f"reward={reward:.2f}")
-        #7
-        drawText(screen, 5, 125, f"numRecords={len(history.records)}")
-        #8
-        drawText(screen, 5, 145, f"state={state}")
+        pf.repopulate()
 
-        cart.draw(screen)
+        res=pf.getResult()
+
+        screen.fill((255, 255, 255))
+        robot.draw(screen)
+        lm.draw(screen)
+        pf.draw(screen)
+
+        pygame.draw.circle(screen, (255, 0, 0), res, 3, 3)
+
+        tree.draw(screen)
+        drawText(screen, f"Test = {1}", 5, 5)
+
         pygame.display.flip()
         timer.tick(fps)
 
-        #
-        # k = abs(self.gamma)
-        # if self.gamma < -0.01:
-        #     self.control = 100 + 0.1 * dxCart * k
-        # elif self.gamma > 0.01:
-        #     self.control = -100 + 0.1 * dxCart * k
+main()
 
-
-#fix reward //account for dgamma_dt
-#fix horizon
-#increase samples count in history
-#filter samples count in history
-#a = v?
+#template file by S. Diane, RTU MIREA, 2024
