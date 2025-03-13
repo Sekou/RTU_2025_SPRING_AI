5a6859bbc5ebb2cb7f85ddc68d979e4509088bbb
Sekou Diane <sekoudiane1990@gmail.com>
2025-03-13 20:48:07 +0300

    pismr pr3 hexapod motion extended 2

--- PISMR/pr3_hexapod/main.py
+++ PISMR/pr3_hexapod/main.py
@@ -3,10 +3,12 @@ from coppeliasim_zmqremoteapi_client import RemoteAPIClient
 TIME=60
 NUM_LEGS=6
 
+
 #управление гексаподом
 print('Program started')
 client = RemoteAPIClient()
 sim = client.getObject('sim')
+t0 = sim.getSimulationTime()
 
 #Prepare initial values and retrieve handles:
 antBase=sim.getObject('./hexapod/base')
@@ -65,14 +67,25 @@ def moveBody():
     p[2]=p[2]+0.03*sizeFactor
     moveToPose(legBase,antBase,p,o,vel*2,accel)
 
-while (t := sim.getSimulationTime()) < TIME:
+while (t := sim.getSimulationTime()) < t0+TIME:
     print(f'Simulation time: {t:.2f}')
     #движение
-    # p = [initialP[0], initialP[1]+t, initialP[2]]
-    # o = [initialO[0], initialO[1], initialO[2]]
-    # moveToPose(legBase, antBase, p, o, vel, accel)
-    # moveToPose()
-    moveBody()
+
+    MODE=2
+
+    if MODE==1:
+        p = [initialP[0], initialP[1]+t, initialP[2]]
+        o = [initialO[0], initialO[1], initialO[2]]
+        moveToPose(legBase, antBase, p, o, vel, accel)
+    # moveBody()
+    if MODE == 2:
+        sim.setStringSignal("IK", "wait")
+        for leg in range(6):
+            p = [initialPos[leg][0] + 0.05*math.sin(t/5),
+                 initialPos[leg][1] + 0.05*math.sin(t/5),
+                 initialPos[leg][2] + 0]
+            sim.setObjectPosition(simLegTargets[leg], p, legBase)
+        sim.setStringSignal("IK", "calc")
 
     client.step()  # triggers next simulation step
 
@@ -147,4 +160,46 @@ print('Program ended')
 #         p[3]=p[3]-0.0*sizeFactor
 #         moveToPose(legBase,antBase,p,o,vel,accel)
 #     end
+# end
+
+#
+# function sysCall_actuation()
+#     dt=sim.getSimulationTimeStep()
+#
+#     dx=movData.strength-realMovementStrength
+#     if (math.abs(dx)>dt*0.1) then
+#         dx=math.abs(dx)*dt*0.5/dx
+#     end
+#     realMovementStrength=realMovementStrength+dx
+#
+#     spsp={}
+#     for leg=1,6,1 d
+#         sp=(stepProgression+(legMovementIndex[leg]-1)/6) % 1
+#         offset={0,0,0}
+#         if (sp<(1/3)) then
+#             offset[1]=sp*3*movData.amplitude/2
+#         else
+#             if (sp<(1/3+1/6)) then
+#                 s=sp-1/3
+#                 offset[1]=movData.amplitude/2-movData.amplitude*s*6/2
+#                 offset[3]=s*6*movData.height
+#             else
+#                 if (sp<(2/3)) then
+#                     s=sp-1/3-1/6
+#                     offset[1]=-movData.amplitude*s*6/2
+#                     offset[3]=(1-s*6)*movData.height
+#                 else
+#                     s=sp-2/3
+#                     offset[1]=-movData.amplitude*(1-s*3)/2
+#                 end
+#             end
+#         end
+#         md=movData.dir+math.abs(movData.rot)*math.atan2(initialPos[leg][1]*movData.rot,-initialPos[leg][2]*movData.rot)
+#         offset2={offset[1]*math.cos(md)*realMovementStrength,offset[1]*math.sin(md)*realMovementStrength,offset[3]*realMovementStrength}
+#         p={initialPos[leg][1]+offset2[1],initialPos[leg][2]+offset2[2],initialPos[leg][3]+offset2[3]}
+#         sim.setObjectPosition(simLegTargets[leg],p,legBase) -- We simply set the desired foot position. IK is handled after that
+#     end
+#     simIK.handleGroup(ikEnv,ikGroup,{syncWorlds=true,allowError=true})
+#
+#     stepProgression=stepProgression+dt*movData.vel
 # end
