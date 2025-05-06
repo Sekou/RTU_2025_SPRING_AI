f43d17d87c8abba7f486e0990a0f1199afb3891d
Sekou Diane <sekoudiane1990@gmail.com>
2025-05-06 16:18:40 +0300

    mimr pr6 signal proc added some functions

--- MIIMR/pr6_signal_proc/main3.py
+++ MIIMR/pr6_signal_proc/main3.py
@@ -1,3 +1,6 @@
+#по материалам статьи
+#https://cyberleninka.ru/article/n/odnomernyy-filtr-kalmana-v-algoritmah-chislennogo-resheniya-zadachi-optimalnogo-dinamicheskogo-izmereniya
+
 import math
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,7 +10,20 @@ xx=np.arange(0, 20, 0.1)
 np.random.seed(0)
 yy=[math.sin(x)+np.random.normal(0,0.1) for x in xx]
 
-y=[]
+yy_mes=[5, 5, 5, 5] #измерения сигнала
+yy_est=[4, 5, 6, 7] #оценки сигнала
+def dev_avg(i, N): #среднее отклонение сигнала по ретроспективному окну N
+    i0, i1 = i-N+1, i+1
+    yy_mes_, yy_est_=yy_mes[i0:i1], yy_est[i0:i1]
+    return np.mean([a-b for (a, b) in zip(yy_mes_, yy_est_)])
+
+d=dev_avg(3, 3)
+print(d)
+
+def dev_dev_avg(i, N):
+    dd=[dev_avg(i-N+1+j, N) for j in range(N)]
+    return np.sum(dd)/(len(dd)+1)
+
 #метод экспоненциального среднего
 def get_kalman_filter_value(yy, i, win_size):
     P=0.1 #if i==0 else
